c499c9482f479c31b6ffe724fa87f58013aa81f7
T-musiz <musiz2511@gmail.com>
2021-11-18 20:58:33 +0700

    commit on master

--- Blackground.js
+++ Blackground.js
@@ -4,7 +4,7 @@ function createElement(name,id,nO,iG,Phone){
     let newNode = document.createElement('div');
     newNode.className = 'row p-3 my-3 member-elm';
     newNode.innerHTML = `<div class="col-sm ms-md ">
-    <img class="member-img" src="./img/01-Pat.jpeg">
+    <img class="member-img" src="${memberRef}">
   </div>
   <div class="col-sm m-auto" style="padding: 25px;" >
     <h4> ${name} </h4>
@@ -14,12 +14,14 @@ function createElement(name,id,nO,iG,Phone){
   </div>`;
   container.appendChild(newNode);
 };
-function GetData(){
-    db.collection("Member").doc('47658').get().then(function(doc){
-        let Data = doc.data();
+function GetData(size){
+  for(i=0;i<=size;i++){  
+    console.log(i)
+  db.collection("Member").where("No","==",i).get().then(function(snapshot){
+    snapshot.forEach(function(docs){
+        let Data = docs.data();
         createElement(Data.EngName,Data.ID,Data.No,Data.Instagram,Data.PhoneNo)
-        console.log(Data)
-    })}
-GetData()
-NoMember= db.collection("Member").get().then(doc => doc.size)
-console.log(NoMember)
+  })})}}
+db.collection("Member").get().then((res) => GetData(res.size))
+var memberRef = storageRef.child('img/01-Pat.jpeg')
+
