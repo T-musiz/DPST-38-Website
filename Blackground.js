728d413a874d288415558c81f707d86b3beadc04
T-musiz <musiz2511@gmail.com>
2021-11-21 04:51:56 +0700

    commit on master

--- Blackground.js
+++ Blackground.js
@@ -1,27 +1,48 @@
 const container=document.getElementById("ClassMember-container")
+var url, Url
+var memberImgRef = storageRef.child('img')
+var memberRef = memberImgRef.child('00-Unknow.png')
 
-function createElement(name,id,nO,iG,Phone){
+db.collection("Member").get().then((res) => GetData(res.size))
+let imgName=(`${String(1).padStart(2, '0')}-${'Pat'}.jpeg`)
+  memberRef = memberImgRef.child(imgName)
+memberRef.getDownloadURL().then(function(url1) {Url = url1})
+
+
+function GetData(size){
+  for(i=0;i<=size;i++){  
+  db.collection("Member").where("No","==",i).get().then(function(snapshot){
+    snapshot.forEach(function(docs){
+        let Data = docs.data();
+        createElement(Data.EngName,Data.ID,Data.No,Data.Instagram,Data.PhoneNo,Data.EngNickName)
+       // memberRef = memberImgRef.child(`${String(Data.No).padStart(2, '0')}-${Data.EngNickName}.jpeg`)
+       // memberRef.getDownloadURL().then(function(url1) {url = url1})
+  })})
+}}
+
+
+function createElement(name,id,nO,iG,Phone,nName){
     let newNode = document.createElement('div');
     newNode.className = 'row p-3 my-3 member-elm';
-    newNode.innerHTML = `<div class="col-sm ms-md ">
-    <img class="member-img" src="${memberRef}">
+    newNode.innerHTML = `<div class="col-sm ms-md">
+    <img class="member-img" src="${Url}">
   </div>
   <div class="col-sm m-auto" style="padding: 25px;" >
-    <h4> ${name} </h4>
+    <h4> ${name} (${nName}) </h4>
     <p><b>No.</b> ${nO}    <b>Student ID:</b> ${id}<br>
       <b>Instargram:</b> ${iG} <br>
       <b>Phone:</b> ${Phone}</p>
   </div>`;
   container.appendChild(newNode);
+  console.log('newnode added')
 };
-function GetData(size){
-  for(i=0;i<=size;i++){  
-    console.log(i)
-  db.collection("Member").where("No","==",i).get().then(function(snapshot){
-    snapshot.forEach(function(docs){
-        let Data = docs.data();
-        createElement(Data.EngName,Data.ID,Data.No,Data.Instagram,Data.PhoneNo)
-  })})}}
-db.collection("Member").get().then((res) => GetData(res.size))
-var memberRef = storageRef.child('img/01-Pat.jpeg')
 
+//function pictureUrl(id,nName){
+//  let imgName=(`${String(id).padStart(2, '0')}-${nName}.jpeg`)
+//  memberRef = memberImgRef.child(imgName)
+//  memberRef.getDownloadURL().then(function(url1) {url = url1; return Url})
+//console.log(`img/${String(id).padStart(2, '0')}-${nName}.jpeg`,url)
+//  return   url
+//}
+
+        
